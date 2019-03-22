5f28f75d185426ad88714c4ed3f69036663e7c3a
Priyankachaudhary2606 <priyanka@smaltandberyl.com>
2019-03-22 13:55:19 +0530

    Author : Priyanka Description : POC to exchange requests & response

--- src/main/java/com/example/webhookwithspring/MyWebhookDto.java
+++ src/main/java/com/example/webhookwithspring/MyWebhookDto.java
@@ -2,19 +2,14 @@ package com.example.webhookwithspring;
 
 public class MyWebhookDto {
 	String fulfillmentText;
-	IntentDto intent;
+	
 	public String getFulfillmentText() {
 		return fulfillmentText;
 	}
 	public void setFulfillmentText(String fulfillmentText) {
 		this.fulfillmentText = fulfillmentText;
 	}
-	public IntentDto getIntent() {
-		return intent;
-	}
-	public void setIntent(IntentDto intent) {
-		this.intent = intent;
-	}
+	
 	public MyWebhookDto() {
 		super();
 		// TODO Auto-generated constructor stub
