a10693b89c601844748ca677cc3d94d6ae1003d9
Priyankachaudhary2606 <priyanka@smaltandberyl.com>
2019-03-22 10:36:08 +0530

    Author : Priyanka Description : POC to exchange requests & response

--- src/main/java/com/example/webhookwithspring/MyController.java
+++ src/main/java/com/example/webhookwithspring/MyController.java
@@ -1,14 +1,36 @@
 package com.example.webhookwithspring;
 
+
+import org.json.JSONObject;
+import org.json.JSONString;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyController {
 
-	@RequestMapping("/myapp")
-	public MyWebhookDto myfunc() {
-		System.out.println("Hello World");
-		return new MyWebhookDto("Hello World");
+	@RequestMapping(value="/myapp",method=RequestMethod.POST,consumes=MediaType.APPLICATION_JSON_VALUE)
+	public String myfunc(@RequestBody String myWebhookRequestDto) {
+		
+	return myWebhookRequestDto;
+		
+//		System.out.println("Hello World"+ myWebhookRequestDto);
+//		try {
+//			JSONObject myjsonObj = new JSONObject(myWebhookRequestDto);
+//			JSONObject queryResult = myjsonObj.getJSONObject("queryResult");
+//			JSONObject intent = queryResult.getJSONObject("intent");
+//			JSONObject parameters = queryResult.getJSONObject("parameters");
+//			//JSONString nameparameter = queryResult.
+//					
+//			System.out.println("JSON QueryText"+ myjsonObj.getString("responseId"));
+//			System.out.println("JSON Object"+ myjsonObj.getJSONObject("queryResult")+ "  "+ intent);
+//		} catch (Exception e) {
+//			System.out.println(e);
+//			// TODO Auto-generated catch block
+//		}
+//		return new MyWebhookDto("Hello World");
 	}
 }
--- src/main/java/com/example/webhookwithspring/MyWebhookDto.java
+++ src/main/java/com/example/webhookwithspring/MyWebhookDto.java
@@ -2,24 +2,30 @@ package com.example.webhookwithspring;
 
 public class MyWebhookDto {
 	String fulfillmentText;
-
+	IntentDto intent;
 	public String getFulfillmentText() {
 		return fulfillmentText;
 	}
-
 	public void setFulfillmentText(String fulfillmentText) {
 		this.fulfillmentText = fulfillmentText;
 	}
-
-	public MyWebhookDto(String fulfillmentText) {
-		super();
-		this.fulfillmentText = fulfillmentText;
+	public IntentDto getIntent() {
+		return intent;
 	}
-
+	public void setIntent(IntentDto intent) {
+		this.intent = intent;
+	}
 	public MyWebhookDto() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	public MyWebhookDto(String fulfillmentText) {
+		super();
+		this.fulfillmentText = fulfillmentText; 
+		this.intent = new IntentDto("myIntentname", "my Intent Display Name");
+	}
+
+	
 	
 	
 }
