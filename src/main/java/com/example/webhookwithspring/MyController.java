3b258cceee2a0b950dde497ae331244b150228dc
Priyankachaudhary2606 <priyanka@smaltandberyl.com>
2019-03-22 12:00:10 +0530

    Author : Priyanka Description : POC to exchange requests & response

--- src/main/java/com/example/webhookwithspring/MyController.java
+++ src/main/java/com/example/webhookwithspring/MyController.java
@@ -2,6 +2,7 @@ package com.example.webhookwithspring;
 
 
 
+import org.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,24 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyController {
 
 	@RequestMapping(value="/myapp",method=RequestMethod.POST,consumes=MediaType.APPLICATION_JSON_VALUE)
-	public String myfunc(@RequestBody String myWebhookRequestDto) {
+	public JSONObject myfunc(@RequestBody String myWebhookRequestDto) {
 		
-	return myWebhookRequestDto;
+		JSONObject myjsonObj = new JSONObject();
 		
-//		System.out.println("Hello World"+ myWebhookRequestDto);
-//		try {
-//			JSONObject myjsonObj = new JSONObject(myWebhookRequestDto);
-//			JSONObject queryResult = myjsonObj.getJSONObject("queryResult");
-//			JSONObject intent = queryResult.getJSONObject("intent");
-//			JSONObject parameters = queryResult.getJSONObject("parameters");
-//			//JSONString nameparameter = queryResult.
-//					
-//			System.out.println("JSON QueryText"+ myjsonObj.getString("responseId"));
-//			System.out.println("JSON Object"+ myjsonObj.getJSONObject("queryResult")+ "  "+ intent);
-//		} catch (Exception e) {
-//			System.out.println(e);
-//			// TODO Auto-generated catch block
-//		}
-//		return new MyWebhookDto("Hello World");
+		System.out.println("Hello World"+ myWebhookRequestDto);
+		try {
+			myjsonObj = new JSONObject(myWebhookRequestDto);
+			JSONObject queryResult = myjsonObj.getJSONObject("queryResult");
+			JSONObject intent = queryResult.getJSONObject("intent");
+			JSONObject parameters = queryResult.getJSONObject("parameters");
+			//JSONString nameparameter = queryResult.
+					
+			System.out.println("JSON QueryText"+ myjsonObj.getString("responseId"));
+			System.out.println("JSON Object"+ myjsonObj.getJSONObject("queryResult")+ "  "+ intent);
+			return myjsonObj;
+		} catch (Exception e) {
+			System.out.println(e);
+			return myjsonObj;
+		}
+		
 	}
 }
